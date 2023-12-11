3d68faf7c56710cf420bf4ce36f7eb9ae64b85a4
Carlos Diaz <72442639+Carlos-Diaz-07@users.noreply.github.com>
2023-12-11 12:22:15 +0000

    .

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,24 +3,38 @@ export default {
   docsRepositoryBase: 'https://github.com/Ibex-CRM/Ibex-docs/blob/master',
   logo: (
     <>
+      <img
+        width="50"
+        height="50"
+        src="https://res.cloudinary.com/montycat/image/upload/v1557750788/Goat/goat_logo.png"
+        alt="logo"
+        className="h-6 w-6 inline mr-2"
+      />
       <span className="text-gray-600 font-normal hidden md:inline">
-      The Goat Agency's Dev Documentation
+        <strong>Ibex: </strong>
+        User Documetation
       </span>
     </>
   ),
   head: (
     <>
-      <title>Goat Dev Docs</title>
+      <title>Ibex User Documentation</title>
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Goat Dev Docs: The Goat Agency's Dev Documentation" />
-      <meta name="og:description" content="Goat Dev Docs: The Goat Agency's Dev Documentation" />
+      <meta name="description" content="Ibex User Documentation" />
+      <meta name="og:description" content="Ibex User Documentation" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://res.cloudinary.com/montycat/image/upload/v1557750788/Goat/goat_logo.png" />
-      <meta name="og:title" content="Goat Dev Docs: The Goat Agency's Dev Documentation" />
-      <meta name="og:image" content="https://res.cloudinary.com/montycat/image/upload/v1557750788/Goat/goat_logo.png" />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/montycat/image/upload/v1557750788/Goat/goat_logo.png"
+      />
+      <meta name="og:title" content="Ibex User Documentation" />
+      <meta
+        name="og:image"
+        content="https://res.cloudinary.com/montycat/image/upload/v1557750788/Goat/goat_logo.png"
+      />
       <meta name="apple-mobile-web-app-title" content="Cabra" />
       <link
         rel="apple-touch-icon"
@@ -55,11 +69,11 @@ export default {
     </>
   ),
   sidebar: {
-     defaultMenuCollapseLevel: 1,
-     toggleButton: true,
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
   },
   footer: {
-    text: <>MIT {new Date().getFullYear()} © Goat Dev Docs.</>
+    text: <>MIT {new Date().getFullYear()} © Ibex User Documentation.</>,
   },
   faviconGlyph: '🐐',
 }
