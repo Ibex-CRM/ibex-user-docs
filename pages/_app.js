de70f2da949e240b1c8b4bd87c80e273bca39cbd
Benkilby <benkilby@gmail.com>
2025-05-02 12:06:40 +0100

    page updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,39 @@
 import 'nextra-theme-docs/style.css'
+import { useEffect } from 'react'
 
 export default function Nextra({ Component, pageProps }) {
+  useEffect(() => {
+    function syncWithParentTheme() {
+      try {
+        const parentTheme = window.parent.document.documentElement.getAttribute('data-theme');
+        document.documentElement.classList.remove('light', 'dark');
+        document.documentElement.classList.add(parentTheme);
+      } catch (e) {
+        console.warn('Unable to access parent theme:', e);
+      }
+    }
+
+    try {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.attributeName === 'data-theme') {
+            syncWithParentTheme();
+          }
+        });
+      });
+
+      observer.observe(window.parent.document.documentElement, {
+        attributes: true,
+        attributeFilter: ['data-theme']
+      });
+
+      syncWithParentTheme();
+
+      return () => observer.disconnect();
+    } catch (e) {
+      console.warn('Unable to observe parent theme changes:', e);
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
